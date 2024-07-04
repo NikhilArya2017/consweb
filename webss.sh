5aea7590b7b203fc5265a92d20884a594d65b1c6
NikhilArya2017 <68007284+NikhilArya2017@users.noreply.github.com>
2024-07-04 18:33:53 +0530

    Create webss.sh

---
+++ webss.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+ 
+# Define the content of the HTML file 
+html_content="<html> 
+<head> 
+<title>Sample HTML Page</title> 
+</head> 
+<body> 
+<h1>Hello, World!</h1> 
+<p>This is a sample HTML page generated using shell scripting.</p> 
+</body> 
+</html>" 
